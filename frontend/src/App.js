211b3fb6e5b99f222dc1ffe5e4f6dd0e1f905f71
Natan-333 <natancruz23@gmail.com>
2023-11-05 01:35:27 -0300

    finalizando front, melhorando formulario

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,18 @@
-import GlobalStyle from "./styles/global";
-import styled from "styled-components";
-import Form from "./components/Form.js";
-import Grid from "./components/Grid";
 import { useEffect, useState } from "react";
-import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import axios from "axios";
+import { toast, ToastContainer } from "react-toastify";
 
-const Container = styled.div`
-  width: 100%;
-  max-width: 800px;
-  margin-top: 20px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 10px;
-`;
+import GlobalStyle from "./styles/global";
 
-const Title = styled.h2``;
+// Service import
+import { api } from './services/api';
+
+// Component import
+import Form from "./components/Form";
+import Grid from "./components/Grid";
+
+// Style import
+import { Container, Title } from './styles'
 
 function App() {
   const [users, setUsers] = useState([]);
@@ -25,8 +20,8 @@ function App() {
 
   const getUsers = async () => {
     try {
-      const res = await axios.get("http://localhost:8800");
-      setUsers(res.data.sort((a, b) => (a.nome > b.nome ? 1 : -1)));
+      const { data } = await api.get("http://localhost:8800");
+      setUsers(data.sort((a, b) => (a.nome > b.nome ? 1 : -1)));
     } catch (error) {
       toast.error(error);
     }
