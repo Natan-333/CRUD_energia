f579e103f2a31eb4c40446eb9706eda85116960b
Natan-333 <natancruz23@gmail.com>
2023-11-05 18:27:10 -0300

    front e formulario finais

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { addUser, deleteUser, getUsers, updateUser } from "../controllers/user.js";
 
-const router = express.Router()
+const router = express.Router() //const
 
 router.get("/", getUsers)
 
